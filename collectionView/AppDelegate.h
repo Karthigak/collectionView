c02db69c29d4fbfb12ac7a5096e8d5293beeb76a
Karthigak <karthiga.k@optisolbusiness.com>
2015-07-27 14:11:44 +0530

    Initial Commit

---
+++ collectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  collectionView
+//
+//  Created by OBS_Macmini on 7/27/15.
+//  Copyright (c) 2015 OptisolBusinessSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
